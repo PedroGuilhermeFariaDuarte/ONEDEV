3bc1aa88fd87aa274ff46fb0aec60eca434d2149
PedroGuilherme <pedro_races@hotmail.com>
2020-10-25 17:42:24 -0300

    [GLOBAL] Interface de login e temas  atualizados

---
+++ packages/Xbox UI/src/data/profiles/index.js
@@ -0,0 +1,108 @@
+const profilesData = [
+    {
+        profile: {
+            avatar_url: "https://ic.pics.livejournal.com/photootchet/13992120/60755/60755_original.jpg",
+            gamertag: "ONEDEV",
+            gamescore: 2500
+        },
+        user: {
+            first_name: "Pedro",
+            last_name: "Guilherme",
+            full_name: "Pedro Guilherme",
+            email: "pedro@teste.com.br"
+        },
+        type_account: "ultimate",
+        history: {
+            last_game: {
+                name_game: "",
+                background: ""
+            },
+            lastest_games: [
+                {
+                    name_game: "",
+                    small_background: "",
+                    large_background: ""
+                }
+            ]
+        },
+        config: {
+            system: {
+                theme: {
+                    name: "--theme-greenCyan",
+                    className: "greenCyan",
+                    colorText: "--text-greenCyan"
+                }
+            }
+        }
+    },
+    {
+        profile: {
+            avatar_url: "https://static.boredpanda.com/blog/wp-content/uploads/2016/09/cute-animal-illustration-sydney-hanson-4-57dbe305629ea__700.jpg",
+            gamertag: "KatarinaA",
+            gamescore: 8320
+        },
+        user: {
+            first_name: "Katarina",
+            last_name: "Silva",
+            full_name: "Katarina Silva",
+            email: "katarina@teste.com.br"
+        },
+        type_account: "gold",
+        history: {
+            last_game: {
+                name_game: "",
+                background: ""
+            },
+            lastest_games: [
+                {
+                    name_game: "",
+                    small_background: "",
+                    large_background: ""
+                }
+            ]
+        },
+        config: {
+            system: {
+                theme: {
+                    name: "--theme-blueMagenta",
+                    className: "blueMagenta"
+                }
+            }
+        }
+    },
+    {
+        profile: {
+            avatar_url: "https://static.boredpanda.com/blog/wp-content/uploads/2016/09/cute-animal-illustration-sydney-hanson-4-57dbe305629ea__700.jpg",
+            gamertag: "OtherUser",
+            gamescore: 8320
+        },
+        user: {
+            first_name: "Katarina",
+            last_name: "Silva",
+            full_name: "Katarina Silva",
+            email: "katarina@teste.com.br"
+        },
+        type_account: "gold",
+        history: {
+            last_game: {
+                name_game: "",
+                background: ""
+            },
+            lastest_games: [
+                {
+                    name_game: "",
+                    small_background: "",
+                    large_background: ""
+                }
+            ]
+        },
+        config: {
+            system: {
+                theme: {
+                    name: "--theme-grayGray",
+                    className: "grayGray"
+                }
+            }
+        }
+    },
+]
---
+++ packages/Xbox UI/src/javascript/login/index.js
@@ -0,0 +1,41 @@
+function handlerLoadProfiles(profilesData) {
+    const container = document.querySelector("#container_profiles");
+    profilesData.map((profile, index) => {
+        return container.innerHTML += (`<div class="card_large" onmouseover="handlerSetTheme(${index})">
+            <div class="group_content">
+                <image src='${profile.profile.avatar_url}'
+                    alt="Avatar image" class="image_avatar" id="item${index}"/>
+            </div>
+            <div class="group_content">
+                <div class="informations_avatar">
+                    <span class="strong">${profile.profile.gamertag}</span>
+                    <span class="info">${profile.user.full_name}</span>
+                    <span class="info"><span class="gamescore"></span> ${profile.profile.gamescore}</span>
+                </div>
+            </div>
+        </div>`)
+    })
+}
+
+function handlerSelectProfile(index_profile) {
+    sessionStorage.setItem("profile", JSON.stringify(profilesData[index_profile]))
+    window.location.href = "./src/pages/home/index.html"
+}
+
+function handlerSetTheme(index_profile) {
+    const body = document.querySelector("body");
+    const profileAvatar = document.querySelector(`#item${index_profile}`);
+    const profileSelected = profilesData[index_profile];
+
+    body.style.background = `var(${profileSelected?.config?.system?.theme?.name})`;
+
+    if (handlerRemoveTheme()) {
+        profileAvatar.classList.add(`${profileSelected?.config?.system?.theme?.className}`)
+    }
+
+}
+
+function handlerRemoveTheme() {
+    document.querySelectorAll("img").forEach(item => item?.setAttribute("class", "image_avatar"))
+    return true;
+}
